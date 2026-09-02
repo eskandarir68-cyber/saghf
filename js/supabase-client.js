4dea2533e0448891dff9ec07a6ab8e19f3244f39
eskandarir68-cyber <eskandari.r68@gmail.com>
2026-09-02 21:42:08 +0330

    Create supabase-client.js

---
+++ js/supabase-client.js
@@ -0,0 +1,9 @@
+// =============================================
+// تنظیمات اصلی پروژه سقف
+// =============================================
+
+const SUPABASE_URL = 'https://kdinolsgikcxhdepzvng.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'sb_publishable_WtkOdgzPsbrm_Yaigaw_Fg_t4SqR-ZO';
+
+const SITE_NAME = 'سقف';
+const SITE_BUILDER = 'کوروش غلامی';
